693232a206c57bd73832a40667a43031adf3b18c
adstah <adam.stachowski.pracainz@gmail.com>
2023-10-11 19:12:06 +0100

    added cam display

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -1,9 +1,11 @@
+import { CameraDisplay } from "@/components/(shared)/camera-display/camera-display";
 import { PanelWindow } from "@/components/panel/panel-window/panel-window";
 
 const Page = () => {
   return (
     <div>
       <PanelWindow />
+      <CameraDisplay />
     </div>
   );
 };
---
+++ components/(shared)/camera-display/camera-display.tsx
@@ -0,0 +1,3 @@
+export const CameraDisplay = () => {
+    return <div className="h-100 d-flex"><img className="mw-100" src="http://localhost:8000/video-feed" /></div>
+}
